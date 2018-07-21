d8a6d5b4df16e9d8d21536b1c82ff9fbd717df35
freewind <nowindlee@gmail.com>
2018-07-21 19:30:41 +0800

    two demos

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -4,9 +4,13 @@ import javafx.geometry.Pos
 import tornadofx.*
 
 class HelloWorld : View() {
-    override val root = hbox {
-        button("Open new window").setOnAction {
+    override val root = vbox {
+        button("replace with another view").setOnAction {
             replaceWith(Another())
+        }
+        label("")
+        button("open new window, but can't close current").setOnAction {
+            Another().openWindow()
         }
     }
 }
@@ -25,9 +29,6 @@ class HelloWorldApp : App(HelloWorld::class, HelloWorldStyle::class)
 
 class Another : View() {
     override val root = hbox {
-        label("new window")
-        label("new window")
-        label("new window")
         label("new window")
     }
 }
